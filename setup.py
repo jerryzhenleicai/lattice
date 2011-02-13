c90a76dd2f8b7ccfd081267b3d246d47a18df2e2
Zhenlei Cai <zcai@zcai-MacPro.(none)>
2011-02-13 02:00:01 -0800

    Include junit.jar in the final EGG package installation

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='lattice',
-      version='0.8',
+      version='0.9',
       description='Java Build tool in Python',
       author='Zhenlei Cai',
       author_email='jpenguin@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
       include_package_data = True,
-      packages=['lattice']
+      packages=['lattice'],
+      package_data = {
+        '': ['*.jar'],
+       }
      )
 
